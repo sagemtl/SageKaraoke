fa61c74d50c14bb5ae7a949a38197e38d79df300
Yu Tong Liu <yutongliuytl@gmail.com>
2020-12-21 21:44:01 -0500

    Added React skeleton application

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import logo from '../assets/logo.png';
+import '../styles/App.css';
 
 function App() {
   return (
@@ -7,7 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit
+          <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
