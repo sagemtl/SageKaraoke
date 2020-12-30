aa2129dd870b37c978b58030c8cad5bb6794ad05
Cindy Chen <cxy2015ca@gmail.com>
2020-12-30 12:57:33 -0500

    more restructuring

--- src/router/container/Router.js
+++ src/router/container/Router.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Home from '../../pages/Home';
+import Preview from '../../pages/Preview';
 
 const Router = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={Home} />
-      <Route exact path="/preview" component={Home} />
+      <Route exact path="/preview/:name" component={Preview} />
     </Switch>
   </BrowserRouter>
 );
