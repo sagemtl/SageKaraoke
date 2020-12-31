f5fdd023ff907646551fb323000fea503b9845b0
Tony <tonyou1999@hotmail.com>
2020-12-31 18:18:30 -0500

    feat: setup react context code

--- src/pages/Home/containers/Home.js
+++ src/pages/Home/containers/Home.js
@@ -1,9 +1,25 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
+import { useGlobalContext } from '../../../global/context';
 
-const Home = () => (
-  <div className="home">
-    <h1>Home</h1>
-  </div>
-);
+const Home = () => {
+  const globalContext = useGlobalContext();
+  const [karaokeState, karaokeDispatch] = globalContext.karaoke;
+
+  return (
+    <div className="home">
+      <h1>Home</h1>
+      <h1>{karaokeState.test}</h1>
+      <button
+        type="button"
+        onClick={() => {
+          karaokeDispatch({ type: 'SET_TEST', payload: karaokeState.test + 1 });
+        }}
+      >
+        Increment
+      </button>
+    </div>
+  );
+};
 
 export default Home;
