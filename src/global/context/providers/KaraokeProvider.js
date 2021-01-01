bf6a8333a933d6041d46a3f4db11d7ad20824409
Tony <36376980+Tony9984@users.noreply.github.com>
2021-01-01 14:43:10 -0500

    feat: setup react context code (#3)

---
+++ src/global/context/providers/KaraokeProvider.js
@@ -0,0 +1,37 @@
+import React, { createContext, useReducer, useContext } from 'react';
+import PropTypes from 'prop-types';
+
+const KaraokeContext = createContext();
+
+// TODO: Add global intitial states here
+
+const initialState = {
+  test: 0,
+};
+
+// TODO: Add global state modifier functions here
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'SET_TEST':
+      return { ...state, test: action.payload };
+    default:
+      return null;
+  }
+};
+
+export const KaraokeContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <KaraokeContext.Provider value={[state, dispatch]}>
+      {children}
+    </KaraokeContext.Provider>
+  );
+};
+
+KaraokeContextProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export const useKaraokeContext = () => useContext(KaraokeContext);
