5ba3520ab3fe78dfe82454e3540610cb08937521
Xinrui Li <xinruili@hotmail.com>
2021-01-01 20:03:13 -0500

    feat: control center buttons

---
+++ src/pages/Home/containers/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Controls from '../../../components/Controls/Controls';
+
+const Home = () => (
+  <div className="home">
+    <h1>Home</h1>
+    <Controls />
+  </div>
+);
+
+export default Home;
