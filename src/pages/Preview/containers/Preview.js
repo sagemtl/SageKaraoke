b0be0deaa1117e4c57c9eb6f152ea6311d510a39
Cindy Chen <cxy2015ca@gmail.com>
2021-01-03 18:39:57 -0500

    connected preview page to backend, queries via title id on page load

--- src/pages/Preview/containers/Preview.js
+++ src/pages/Preview/containers/Preview.js
@@ -1,22 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
+import ReactPlayer from 'react-player';
+import { getSongByTitleId } from 'utils/ktvQueries';
 
 const Preview = ({ match }) => {
   const {
     params: { songName },
   } = match;
 
+  const [songTitle, setSongTitle] = useState('');
+  const [artist, setArtist] = useState('');
+
+  useEffect(() => {
+    const getSongInfo = async () => {
+      const songInfo = await getSongByTitleId(songName);
+      console.log(songInfo);
+      setSongTitle(songInfo.title);
+      setArtist(songInfo.artist);
+    };
+
+    getSongInfo();
+  }, []);
+
   return (
     <div className="home">
       <h1>Preview page</h1>
-      <h3>Playing {songName}</h3>
-      <video
-        width="320"
-        height="240"
-        src="../../../assets/moon-represent-my-heart-vid.mp4"
-      >
-        Sorry, your browser doesnt support embedded videos.
-      </video>
+      <h3>Playing {songTitle}</h3>
+      <h3>By {artist}</h3>
+      <ReactPlayer url="assets/moon-represent-my-heart-vid.mp4" playing>
+        {/* Sorry, your browser doesnt support embedded videos. */}
+      </ReactPlayer>
     </div>
   );
 };
