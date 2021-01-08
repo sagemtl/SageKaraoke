61dee82d9e4ccf803641c2f47a1ad94a98031a06
Yu Tong Liu <yutongliuytl@gmail.com>
2021-01-08 17:27:24 -0500

    Restructured AudioAnalyser folder

--- src/components/AudioAnalyser/containers/AudioInput.js
+++ src/components/AudioAnalyser/containers/AudioInput.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import AudioAnalyser from './AudioAnalyser';
+import AudioAnalyser from '../components/AudioAnalyser';
 
 const AudioInput = ({ songTitle }) => {
   const [audio, setAudio] = useState(null);
