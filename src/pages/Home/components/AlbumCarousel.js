94dd3437ac8e5e9be209e88cd7bb6069de3d9db5
Tony <tonyou1999@hotmail.com>
2021-01-09 15:20:09 -0500

    feat: final style changes for first version

--- src/pages/Home/components/AlbumCarousel.js
+++ src/pages/Home/components/AlbumCarousel.js
@@ -1,68 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Carousel from 'react-spring-3d-carousel';
 import AlbumCover1 from 'assets/albumCover1.jpg';
+import { getAllSongs } from 'utils/ktvQueries';
 
 const AlbumCarousel = () => {
-  const [album, setAlbum] = useState(0);
+  const [selectedAlbum, setSelectedAlbum] = useState(0);
+  const [albums, setAlbums] = useState([]);
 
-  const albums = [
-    {
-      title: 'The Moon Represents my Heart',
-      artist: 'Teresa Teng',
-      coverImage: AlbumCover1,
-    },
-    {
-      title: 'Friends',
-      artist: 'Emil Wakin Chau',
-      coverImage: AlbumCover1,
-    },
-    {
-      title: 'Take me to your heart',
-      artist: 'Michael Learns to Rock',
-      coverImage: AlbumCover1,
-    },
-    {
-      title: 'Red Bean',
-      artist: 'Faye Wong',
-      coverImage: AlbumCover1,
-    },
-    {
-      title: 'Yi Jian Mei',
-      artist: 'Fei Yu Ching',
-      coverImage: AlbumCover1,
-    },
-    {
-      title: '光辉岁月',
-      artist: 'Beyond',
-      coverImage: AlbumCover1,
-    },
-    {
-      title: '上海滩',
-      artist: '叶丽仪',
-      coverImage: AlbumCover1,
-    },
-    {
-      title: 'Ju Hua Tai',
-      artist: 'Jay Chou',
-      coverImage: AlbumCover1,
-    },
-    {
-      title: 'Nobody',
-      artist: 'Wonder Girls',
-      coverImage: AlbumCover1,
-    },
-    {
-      title: 'Tong Hua',
-      artist: 'Michael',
-      coverImage: '',
-    },
-  ];
+  useEffect(() => {
+    getAllSongs().then((res) => {
+      setAlbums(res);
+    });
+  }, []);
 
   const transformData = (data) => {
     const content = data.map((slide, index) => ({
       key: index,
       content: (
-        <div className="album">
+        <div className={selectedAlbum === index ? 'album--selected' : 'album'}>
           <h1 className="album__title">{slide.title}</h1>
           <p className="album__subtitle">{slide.artist}</p>
           <img src={AlbumCover1} alt="test" className="album__cover" />
@@ -72,7 +27,7 @@ const AlbumCarousel = () => {
 
     return content.map((slide, index) => ({
       ...slide,
-      onClick: () => setAlbum(index),
+      onClick: () => setSelectedAlbum(index),
     }));
   };
 
@@ -80,7 +35,7 @@ const AlbumCarousel = () => {
     <div style={{ width: '80%', height: '500px', margin: '0 auto' }}>
       <Carousel
         slides={transformData(albums)}
-        goToSlide={album}
+        goToSlide={selectedAlbum}
         offsetRadius={2}
       />
     </div>
