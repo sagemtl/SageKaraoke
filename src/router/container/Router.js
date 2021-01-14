6eeaf7ff089e386edb32e967c999d9178c3daf6e
Xinrui Li <xinruili@hotmail.com>
2021-01-13 21:12:50 -0500

    fix: router and layout

--- src/router/container/Router.js
+++ src/router/container/Router.js
@@ -1,18 +1,19 @@
 import React from 'react';
-import { BrowserRouter, Switch } from 'react-router-dom';
-
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Preview from 'pages/Preview';
 import Home from 'pages/Home';
 import Sing from 'pages/Sing';
-import RouteWrapper from '../components/RouteWrapper';
+import Layout from 'global/containers/Layout';
 
 const Router = () => (
   <BrowserRouter>
-    <Switch>
-      <RouteWrapper exact path="/" component={Home} />
-      <RouteWrapper exact path="/preview/:songName" component={Preview} />
-      <RouteWrapper exact path="/sing/:songTitle" component={Sing} />
-    </Switch>
+    <Layout>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/preview/:songName" component={Preview} />
+        <Route exact path="/sing/:songTitle" component={Sing} />
+      </Switch>
+    </Layout>
   </BrowserRouter>
 );
 
