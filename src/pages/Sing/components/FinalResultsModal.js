fde796998c59a4dedf2e30779d8f0a6f81533f6b
Cindy Chen <cxy2015ca@gmail.com>
2021-02-23 21:42:01 -0500

    added form, need styling

--- src/pages/Sing/components/FinalResultsModal.js
+++ src/pages/Sing/components/FinalResultsModal.js
@@ -2,6 +2,7 @@
 import { useGlobalContext } from 'global/context';
 import React, { useState, useEffect } from 'react';
 import Dialog from '@material-ui/core/Dialog';
+// import TextField from '@matesrial-ui/core/TextField';
 import Slide from '@material-ui/core/Slide';
 import { useHistory } from 'react-router-dom';
 import ScoreRenderer from './ScoreRenderer';
@@ -17,6 +18,7 @@ const FinalResultsModal = () => {
   const [karaokeState, karaokeDispatch] = globalContext.karaoke;
   const { pitchScore, audioEnded, lyricsScore } = karaokeState;
   const [modal, setModal] = useState(false);
+  const [nameLeaderboard, setNameLeaderboard] = useState('');
   const history = useHistory();
 
   const closeModal = () => {
@@ -42,9 +44,14 @@ const FinalResultsModal = () => {
     return Math.round(lyricsScore);
   };
 
+  const submitLeaderboard = (event) => {
+    console.log(`entered name ${nameLeaderboard}`);
+    event.preventDefault();
+  };
+
   useEffect(() => {
     console.log(audioEnded);
-    if (audioEnded) {
+    if (true) {
       setModal(true);
     }
   }, [audioEnded]);
@@ -80,6 +87,22 @@ const FinalResultsModal = () => {
             alt="Your Score Is"
           />
           <ScoreRenderer number={getFinalScore()} />
+          <div className="leaderboard-info">
+            To display on the leaderboard, please enter your name
+          </div>
+          <form onSubmit={(e) => submitLeaderboard(e)}>
+            <label htmlFor="nameLeaderboard">
+              Name:
+              <input
+                id="nameLeaderboard"
+                type="text"
+                name="name"
+                value={nameLeaderboard}
+                onChange={(e) => setNameLeaderboard(e.target.value)}
+              />
+            </label>
+            <input type="submit" value="Submit" />
+          </form>
           <div className="buttons">
             <button
               className="return-home-btn"
