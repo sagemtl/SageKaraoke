101c4a5b35408d03bc2f4a599703440ea533a141
Yu Tong Liu <yutongliuytl@gmail.com>
2022-02-23 22:49:13 -0500

    Prettier fix

--- src/pages/NotFound/containers/NotFound.js
+++ src/pages/NotFound/containers/NotFound.js
@@ -3,11 +3,7 @@ import '../styles/notFound.scss';
 
 const NotFound = () => (
   <div className="not-found">
-    <img
-      src="/404-octopus.png"
-      alt="404 Not Found"
-      className="octopus-image"
-    />
+    <img src="/404-octopus.png" alt="404 Not Found" className="octopus-image" />
     <div className="not-found-title">404 | NOT FOUND</div>
     <p className="not-found-content">
       You just hit a route that doesn&apos;t exist... the sadness.
